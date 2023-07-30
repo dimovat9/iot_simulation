3532c0593f53b8e0f8ddc60c9679fba8eb8a8ce2
dimovat9 <dimovatanja9@gmail.com>
2023-07-30 13:18:42 +0200

    trigger pipeline

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
-// Replace 'mongodb://localhost/iot_simulation' with your MongoDB connection string
+//Replace 'mongodb://localhost/iot_simulation' with your MongoDB connection string
 mongoose.connect('mongodb://localhost/iot_simulation', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
